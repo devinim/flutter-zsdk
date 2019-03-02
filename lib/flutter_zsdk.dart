f3b953f54f2cf62518ab2ec11e608d1bd70c5a1a
Furkan <furkanbulut@gmail.com>
2019-03-02 13:53:20 +0300

    changes on example app

--- lib/flutter_zsdk.dart
+++ lib/flutter_zsdk.dart
@@ -2,30 +2,43 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+class ZebraBluetoothDevice {
+  final String mac;
+  String friendlyName = "Unknown";
+
+  ZebraBluetoothDevice(this.mac, this.friendlyName);
+
+  Future<Map<String, Map<String, String>>> properties() => FlutterZsdk.getDeviceProperties(mac);
+
+  Future<void> sendZplOverBluetooth(String data) => FlutterZsdk.sendZplOverBluetooth(mac, data);
+}
+
 class FlutterZsdk {
   static const MethodChannel _channel = const MethodChannel('flutter_zsdk');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-
-  static Future<Map<String, String>> discoverBluetoothDevices() async {
+  static Future<List<ZebraBluetoothDevice>> discoverBluetoothDevices() async {
     dynamic d = await _channel.invokeMethod("discoverBluetoothDevices");
-    return Map<String, String>.from(d);
+
+    List<ZebraBluetoothDevice> devices = List();
+
+    d.forEach((k, v) {
+      devices.add(ZebraBluetoothDevice(k, v));
+    });
+
+    return devices;
   }
 
   static Future<Map<String, Map<String, String>>> getDeviceProperties(String mac) async {
     dynamic d = await _channel.invokeMethod("getDeviceProperties", {"mac": mac});
     Map<String, Map<String, String>> map = Map();
 
-    d.foreach((k, v) {
+    d.forEach((k, v) {
       map[k] = Map<String, String>.from(v);
     });
     return map;
   }
 
   static Future<void> sendZplOverBluetooth(String mac, String data) async {
-     await _channel.invokeMethod("sendZplOverBluetooth", {"mac": mac, "data": data});
+    await _channel.invokeMethod("sendZplOverBluetooth", {"mac": mac, "data": data});
   }
 }
