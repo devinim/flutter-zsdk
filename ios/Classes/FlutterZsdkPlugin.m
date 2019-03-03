b4882cbdb9ae1b463292cee42f9cd4c9639d37ec
Furkan <devinim@Furkans-iMac.local>
2019-03-03 13:16:36 +0300

    battery status for ios

--- ios/Classes/FlutterZsdkPlugin.m
+++ ios/Classes/FlutterZsdkPlugin.m
@@ -3,6 +3,7 @@
 #import "MfiBtPrinterConnection.h"
 #import "ZebraPrinter.h"
 #import "ZebraPrinterFactory.h"
+#import <SGD.h>
 
 @implementation FlutterZsdkPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -15,99 +16,113 @@
 
 
 - (void)discoverBluetoothDevices:(FlutterResult)result {
-    NSString *serialNumber = @"";
-    NSString *name = @"";
-    
-    NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
-    
-    EAAccessoryManager *sam = [EAAccessoryManager sharedAccessoryManager];
-    NSArray * connectedAccessories = [sam connectedAccessories];
-    for (EAAccessory *accessory in connectedAccessories) {
-        if([accessory.protocolStrings indexOfObject:@"com.zebra.rawport"] != NSNotFound){
-            serialNumber = accessory.serialNumber;
-            name = accessory.name;
-            
-            [dict setObject:name forKey:serialNumber];
+    @try {
+        NSString *serialNumber = @"";
+        NSString *name = @"";
+        
+        NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
+        
+        EAAccessoryManager *sam = [EAAccessoryManager sharedAccessoryManager];
+        NSArray * connectedAccessories = [sam connectedAccessories];
+        for (EAAccessory *accessory in connectedAccessories) {
+            if([accessory.protocolStrings indexOfObject:@"com.zebra.rawport"] != NSNotFound){
+                serialNumber = accessory.serialNumber;
+                name = accessory.name;
+                
+                [dict setObject:name forKey:serialNumber];
+            }
         }
+        result(dict);
     }
-    result(dict);
+    @catch (NSException *exception) {
+        result([FlutterError errorWithCode:@"Error"
+                                       message: exception.reason
+                                       details:nil]);
+    }
 }
 
 - (void)getDeviceProperties:(NSString*) serial result:(FlutterResult)result {
      NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
-     [dict setObject:@"not" forKey:@"avaiable"];
+     [dict setObject:@"Not Implemented" forKey:@"error"];
      result(dict);
+}
+
+- (void)getBatteryLevel:(NSString*) serial result:(FlutterResult)result {
+  
+    @try {
+        id<ZebraPrinterConnection, NSObject> connection = [[MfiBtPrinterConnection alloc] initWithSerialNumber:serial];
+        
+        NSError *error = nil;
+        
+        [connection open];
+        
+        NSString *battery = [SGD GET:@"power.percent_full" withPrinterConnection:connection error:&error];
+        
+        [connection close];
+        
+        if (error != nil) {
+            @throw [NSException exceptionWithName:@"Printer Error"
+                                           reason:[error description]
+                                           userInfo:nil];
+        }
+        
+        result(battery);
+    }
+    @catch (NSException *exception) {
+        result([FlutterError errorWithCode:@"Error"
+                                   message: exception.reason
+                                   details:nil]);
+    }
+    
 }
 
 - (void)sendZplOverBluetooth:(NSString *)serial data:(NSString*)data result:(FlutterResult)result {
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-       
-        // Instantiate connection to Zebra Bluetooth accessory
-        EAAccessoryManager *sam = [EAAccessoryManager sharedAccessoryManager];
+        @try {
+            // Instantiate connection to Zebra Bluetooth accessory
+            //EAAccessoryManager *sam = [EAAccessoryManager sharedAccessoryManager];
+            
+            id<ZebraPrinterConnection, NSObject> connection = [[MfiBtPrinterConnection alloc] initWithSerialNumber:serial];
+            
+            NSError *error = nil;
+            
+            BOOL success = [connection open];
+            
+            /*
+             id<ZebraPrinter,NSObject> printer = [ZebraPrinterFactory getInstance:connection error:&error];
+            */
+            NSData *dataBytes = [NSData dataWithBytes:[data UTF8String] length:[data length]];
+            
+            [connection write:dataBytes error:&error];
+            
+            /*
+             success = success && [thePrinterConn write:[data dataUsingEncoding:NSUTF8StringEncoding] error:&error];
+             */
+            if (error != nil) {
+                @throw [NSException exceptionWithName:@"Printer Error"
+                                               reason:[error description]
+                                               userInfo:nil];
+            } else {
+                result(@"Wrote. Are you happy?");
+            }
         
-        id<ZebraPrinterConnection, NSObject> connection = [[MfiBtPrinterConnection alloc] initWithSerialNumber:serial];
-        
-        NSError *error = nil;
-        
-        BOOL success = [connection open];
-        
-        id<ZebraPrinter,NSObject> printer = [ZebraPrinterFactory getInstance:connection error:&error];
-        
-        NSData *dataBytes = [NSData dataWithBytes:[data UTF8String] length:[data length]];
-        
-        [connection write:dataBytes error:&error];
-        
-        /* success = success && [thePrinterConn write:[data dataUsingEncoding:NSUTF8StringEncoding] error:&error];*/
-        if (success != YES || error != nil) {
-            result([FlutterError errorWithCode:@"Error"
-                                       message: error.description
-                                       details:nil]);
-        }
-        
-        [connection close];
-        result(@"Wrote. Are you happy?");
+            [connection close];
+        }@catch (NSException *exception) {
+                result([FlutterError errorWithCode:@"Error"
+                                           message: exception.reason
+                                           details:nil]);
+         }
        // [connection release];
-        
-        
     });
     
 }
 
-/*
--(void)test:(FlutterResult) result {
-    NSString *serialNumber = @"";
-    //Find the Zebra Bluetooth Accessory
-    EAAccessoryManager *sam = [EAAccessoryManager sharedAccessoryManager];
-    NSArray * connectedAccessories = [sam connectedAccessories];
-    for (EAAccessory *accessory in connectedAccessories) {
-        if([accessory.protocolStrings indexOfObject:@"com.zebra.rawport"] != NSNotFound){
-            serialNumber = accessory.serialNumber;
-            break;
-            //Note: This will find the first printer connected! If you have multiple Zebra printers connected, you should display a list to the user and have him select the one they wish to use
-        }
-    }
-    // Instantiate connection to Zebra Bluetooth accessory
-    id<ZebraPrinterConnection, NSObject> thePrinterConn = [[MfiBtPrinterConnection alloc] initWithSerialNumber:serialNumber];
-    // Open the connection - physical connection is established here.
-    BOOL success = [thePrinterConn open];
-    // This example prints "This is a ZPL test." near the top of the label.
-    NSString *zplData = @"^XA^FO20,20^A0N,25,25^FDThis is a ZPL test.^FS^XZ";
-    NSError *error = nil;
-    // Send the data to printer as a byte array.
-    success = success && [thePrinterConn write:[zplData dataUsingEncoding:NSUTF8StringEncoding] error:&error];
-    if (success != YES || error != nil) {
-       result(@"Wrote. Are you happy?");
-    }
-    // Close the connection to release resources.
-    [thePrinterConn close];
-   // [thePrinterConn release];
-      result(@"Wrote. Are you happy?");
-}
- */
+
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-    NSLog(@"Handle method call");
+   NSLog(@"Handle method call");
+    
    if ([@"discoverBluetoothDevices" isEqualToString:call.method]) {
        [self discoverBluetoothDevices:result];
    }
@@ -118,6 +133,13 @@
        
        [self getDeviceProperties:serial result:result];
    }
+   else if ([@"getBatteryLevel" isEqualToString:call.method]) {
+       NSDictionary *arguments = [call arguments];
+       
+       NSString* serial = arguments[@"mac"];
+       
+       [self getBatteryLevel:serial result:result];
+   }
    else if ([@"sendZplOverBluetooth" isEqualToString:call.method]) {
        NSDictionary *arguments = [call arguments];
        
@@ -125,7 +147,6 @@
        NSString *data = arguments[@"data"];
        
        [self sendZplOverBluetooth:serial data:data result:result];
-      // [self test:result];
    }
    else {
      result(FlutterMethodNotImplemented);
